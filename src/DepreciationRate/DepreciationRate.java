f5a7781a1eb85f3e75ed13da4b89d3171832a01d
Kelly Meyer <kellymeyer833@gmail.com>
2021-10-11 17:36:02 +0200

    Initial commit

---
+++ src/DepreciationRate/DepreciationRate.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DepreciationRate;
+
+/**
+ *
+ * @author Kelly Meyer
+ */
+public class DepreciationRate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmMain frmM = new frmMain();
+        frmM.setVisible(true);
+        frmM.setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
+    }
+    
+}
